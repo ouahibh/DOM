ac5924969f66599ebc1a4ca032d354527d3f6d9a
ouahibh <sofiane.h.06.b@gmail.com>
2020-12-12 15:26:59 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+
+
+function createCard(title, content) {
+    let all = document.getElementById('ctr');
+    let card = document.createElement('div');
+    card.setAttribute('class', 'card');
+
+    let top = document.createElement('div');
+    top.setAttribute('class', 'top')
+    let titleCard = document.createElement('span')
+    titleCard.setAttribute('class', 'title-card')
+    titleCard.innerHTML = title
+    top.appendChild(titleCard);
+
+    let moin = document.createElement('div')
+    moin.innerHTML = '<span>-</span>'
+    moin.setAttribute('class', 'moin')
+    moin.setAttribute('onclick', 'deleteTag(this)')
+    top.appendChild(moin);
+    card.appendChild(top);
+
+    let messageContainer = document.createElement('div')
+    messageContainer.setAttribute('class', 'message-container')
+
+    let message = document.createElement('span')
+    message.innerHTML = content
+    message.setAttribute('class', 'message')
+    messageContainer.appendChild(message);
+
+
+    card.appendChild(messageContainer)
+    all.appendChild(card);
+    
+}
+
+function create() {
+    var title = prompt('Titre : ')
+    var message = prompt('Messsage : ')
+    createCard(title, message)
+}
+
+function deleteTag(tag){
+    let card = tag.parentNode.parentNode;
+ card.remove()
+}
